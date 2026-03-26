0c7852052d9cd3da2439b969728cb350aac2405b
aney0714 <gimhh770@gmail.com>
2026-03-26 18:56:20 +0900

    [feat]시작페이지 이미지 추가

--- CampusMarket/app/src/main/java/com/example/campusmarket/StartGetInfo.kt
+++ CampusMarket/app/src/main/java/com/example/campusmarket/StartGetInfo.kt
@@ -1,30 +1,17 @@
 package com.example.campusmarket
 
-
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-
-
-class StartActivity : AppCompatActivity() {
+class StartGetInfo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_start)
-
-        val root = findViewById<android.view.View>(R.id.main)
-
-        // 👉 화면 터치 시 이동
-        root.setOnClickListener {
-            val intent = Intent(this, StartGetInfo::class.java)
-            startActivity(intent)
-        }
-
-        ViewCompat.setOnApplyWindowInsetsListener(root) { v, insets ->
+        setContentView(R.layout.activity_start_get_info)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
