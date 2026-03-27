799b720c73e07505059c57ca7a129b6c3228c333
aney0714 <gimhh770@gmail.com>
2026-03-27 18:49:18 +0900

    [feat]메인 상점 페이지 구현

--- CampusMarket/app/src/main/java/com/example/campusmarket/MarketActivity.kt
+++ CampusMarket/app/src/main/java/com/example/campusmarket/MarketActivity.kt
@@ -1,6 +1,8 @@
 package com.example.campusmarket
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.PopupMenu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,32 @@ import androidx.core.view.WindowInsetsCompat
 class MarketActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_market)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnSort = findViewById<Button>(R.id.btnSort)
+
+        btnSort.setOnClickListener {
+            val popup = PopupMenu(this, btnSort)
+            popup.menuInflater.inflate(R.menu.menu_sort, popup.menu)
+
+            popup.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.sort_popular -> btnSort.text = "인기순"
+                    R.id.sort_latest -> btnSort.text = "최신순"
+                    R.id.sort_price_low -> btnSort.text = "조회순"
+                }
+                true
+            }
+
+            popup.show()
         }
     }
 }
