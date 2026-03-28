02d39b44b7cdf6a60783082bacde05e3194156ae
aney0714 <gimhh770@gmail.com>
2026-03-28 23:06:02 +0900

    [feat]스타트 페이지 버튼 연결 및 웹지도 삽입

--- CampusMarket/app/src/main/java/com/example/campusmarket/StartGetInfo.kt
+++ CampusMarket/app/src/main/java/com/example/campusmarket/StartGetInfo.kt
@@ -1,7 +1,11 @@
 package com.example.campusmarket
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class StartGetInfo : AppCompatActivity() {
+
+    private lateinit var webViewMap: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,12 +28,47 @@ class StartGetInfo : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val btnSkip = findViewById<Button>(R.id.tvSkip)
+
+        btnSkip.setOnClickListener {
+            val intent = Intent(this, MarketActivity::class.java)
+            startActivity(intent)
+        }
+
+
 
         val btnNext = findViewById<Button>(R.id.btnNext)
+        webViewMap = findViewById(R.id.webViewMap)
+
+        webViewMap.settings.javaScriptEnabled = true
+        webViewMap.settings.domStorageEnabled = true
+        webViewMap.settings.useWideViewPort = true
+        webViewMap.settings.loadWithOverviewMode = true
+        webViewMap.settings.setSupportZoom(true)
+        webViewMap.settings.builtInZoomControls = true
+        webViewMap.settings.displayZoomControls = false
+
+        webViewMap.webViewClient = WebViewClient()
+        webViewMap.webChromeClient = WebChromeClient()
+
+        webViewMap.loadUrl("https://map-web-sigma.vercel.app/")
 
         btnNext.setOnClickListener {
             val intent = Intent(this, MarketActivity::class.java)
             startActivity(intent)
         }
     }
+
+
+    override fun onBackPressed() {
+        if (::webViewMap.isInitialized && webViewMap.canGoBack()) {
+            webViewMap.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+
+
+
 }
